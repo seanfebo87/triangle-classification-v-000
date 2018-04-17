f57ee3a44058aef6fda6ce284d5d267c3baa893a
Sean Febo <seanfebo@techie.com>
2018-04-17 01:47:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+  def initialize(length_1, length_2, length_3)
+    @length_3 = length_3
+    @length_2 = length_2
+    @length_1 = length_1
+  end
+  def kind 
+    if length_1 == length_2 && length_2 == length_3
+      :equilateral
+    elsif length_1 != length_2 && length_2 != length_3 && length_1 != length_3
+      :scalene
+    else
+      :isosceles
+    end
+  end
+  
 end
+
+class TriangelError
+  
