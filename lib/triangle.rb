a643230ab0534816a4948e6cf20b2638d771ec47
Sean Febo <seanfebo@techie.com>
2018-04-17 01:56:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,20 +7,22 @@ class Triangle
   end
   def kind 
     if side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2
-      raise TriangelError
+      raise TriangleError
     if side1 > 0 && side2 > 0 && side3 > 0
     if length_1 == length_2 && length_2 == length_3
       :equilateral
-      
+      raise TriangleError
     elsif length_1 != length_2 && length_2 != length_3 && length_1 != length_3
       :scalene
     else
       :isosceles
     end
+  else 
+    raise TriangleError
   end
   
 end
 
-class TriangelError < StandardError
+class TriangleError < StandardError
   "This is not a valid Triangle.
   
