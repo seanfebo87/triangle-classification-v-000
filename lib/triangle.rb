bb58169fe8f83535b2bf5ec91e4a1083d1bc589e
Sean Febo <seanfebo@techie.com>
2018-04-17 02:00:52 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
     if side1 > 0 && side2 > 0 && side3 > 0
     if side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2
       raise TriangleError
-    if length_1 == length_2 && length_2 == length_3
+    elsif length_1 == length_2 && length_2 == length_3
       :equilateral
       raise TriangleError
     elsif length_1 != length_2 && length_2 != length_3 && length_1 != length_3
